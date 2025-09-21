1ef23ab317236655dcfa60565daa3f467e055145
n-nob-at-GitHub <sub.nakamura.xyz@gmail.com>
2025-09-21 19:24:20 +0900

    全社用簡易Webページ対応（ダッシュボードタブを在庫管理タブに変更し、詳細はメニュー化した、具体的なメニュー内容は未実装）

--- simple-busininal-webpage/src/components/Contents.tsx
+++ simple-busininal-webpage/src/components/Contents.tsx
@@ -1,5 +1,6 @@
 'use client'
 import {
+  MouseEvent,
   ReactNode,
   SyntheticEvent,
   useState
@@ -24,6 +25,7 @@ interface TabPanelProps {
   value: number;
 }
 
+type StockKey = '在庫一覧' | '入庫' | '出庫' | '入庫履歴' | '出庫履歴'
 type MasterKey = '品目' | '製造メーカー' | '配送先' | 'ユーザー' | 'ロール' 
 
 const CustomTabPanel = (props: TabPanelProps) => {
@@ -50,10 +52,22 @@ const a11yProps = (index: number) => {
 
 const Contents = () => {
   const [ tabIndex, setTabIndex ] = useState(0);
+  const [ stockMenu, setStockMenu ] = useState<StockKey | null>(null)
   const [ masterMenu, setMasterMenu ] = useState<MasterKey | null>(null)
-  const [ anchorElement, setAnchorElement ] = useState<null | HTMLElement>(null)
-  const handleMasterTabClick = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElement(event.currentTarget)
+  const [ stockAnchorElement, setStockAnchorElement ] = useState<null | HTMLElement>(null)
+  const [ masterAnchorElement, setMasterAnchorElement ] = useState<null | HTMLElement>(null)
+  const handleStockTabClick = (event: MouseEvent<HTMLElement>) => {
+    setStockAnchorElement(event.currentTarget)
+  }
+  const handleMasterTabClick = (event: MouseEvent<HTMLElement>) => {
+    setMasterAnchorElement(event.currentTarget)
+  }
+  const stockComponents: Record<StockKey, ReactNode> = {
+    '在庫一覧': <>TODO: 在庫一覧</>,
+    '入庫': <>TODO: 入庫</>,
+    '出庫': <>TODO: 出庫</>,
+    '入庫履歴': <>TODO: 入庫履歴</>,
+    '出庫履歴': <>TODO: 出庫履歴</>,
   }
   const masterComponents: Record<MasterKey, ReactNode> = {
     '品目': <Materials />,
@@ -63,43 +77,60 @@ const Contents = () => {
     'ロール': <Roles />,
   }
   const handleTabChange = (event: SyntheticEvent, newTabIndex: number) => {
-    if (newTabIndex === 1) {
-      setAnchorElement(event.currentTarget as HTMLElement)
+    if (newTabIndex === 0) {
+      setStockAnchorElement(event.currentTarget as HTMLElement)
+    } else if (newTabIndex === 1) {
+      setMasterAnchorElement(event.currentTarget as HTMLElement)
     } else {
       setTabIndex(newTabIndex)
+      setStockMenu(null)
       setMasterMenu(null)
     }
+  }
+  const handleStockMenuSelect = (menu: StockKey) => {
+    setStockMenu(menu)
+    setTabIndex(0)
+    setStockAnchorElement(null)
   }
   const handleMenuSelect = (menu: MasterKey) => {
     setMasterMenu(menu)
     setTabIndex(1)
-    setAnchorElement(null)
-  }
-  const handleMenuClose = () => {
-    setAnchorElement(null)
+    setMasterAnchorElement(null)
   }
   return (
     <Box sx={{ width: '100%' }}>
       <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
         <Tabs value={ tabIndex } onChange={ handleTabChange } variant='scrollable' scrollButtons='auto' textColor='secondary' indicatorColor='secondary' aria-label='basic tabs example'>
-          <Tab label='ダッシュボード' { ...a11yProps(0) } />
+          <Tab label='在庫管理' { ...a11yProps(0) } onClick={ handleStockTabClick } />
           <Tab label='マスタ' { ...a11yProps(1) } onClick={ handleMasterTabClick } />
           <Tab label='ライセンス' { ...a11yProps(2) } />
         </Tabs>
       </Box>
       <Menu
-        anchorEl={ anchorElement }
-        open={ Boolean(anchorElement) }
-        onClose={ handleMenuClose }
+        anchorEl={ stockAnchorElement }
+        open={ Boolean(stockAnchorElement) }
+        onClose={ () => setStockAnchorElement(null) }
       >
-        <MenuItem onClick={() => handleMenuSelect('品目')}>品目</MenuItem>
-        <MenuItem onClick={() => handleMenuSelect('製造メーカー')}>製造メーカー</MenuItem>
-        <MenuItem onClick={() => handleMenuSelect('配送先')}>配送先</MenuItem>
-        <MenuItem onClick={() => handleMenuSelect('ユーザー')}>ユーザー</MenuItem>
-        <MenuItem onClick={() => handleMenuSelect('ロール')}>ロール</MenuItem>
+        <MenuItem onClick={ () => handleStockMenuSelect('在庫一覧') }>在庫一覧</MenuItem>
+        <MenuItem onClick={ () => handleStockMenuSelect('入庫') }>入庫</MenuItem>
+        <MenuItem onClick={ () => handleStockMenuSelect('出庫') }>出庫</MenuItem>
+        <MenuItem onClick={ () => handleStockMenuSelect('入庫履歴') }>入庫履歴</MenuItem>
+        <MenuItem onClick={ () => handleStockMenuSelect('出庫履歴') }>出庫履歴</MenuItem>
+      </Menu>
+      <Menu
+        anchorEl={ masterAnchorElement }
+        open={ Boolean(masterAnchorElement) }
+        onClose={ () => setMasterAnchorElement(null) }
+      >
+        <MenuItem onClick={ () => handleMenuSelect('品目') }>品目</MenuItem>
+        <MenuItem onClick={ () => handleMenuSelect('製造メーカー') }>製造メーカー</MenuItem>
+        <MenuItem onClick={ () => handleMenuSelect('配送先') }>配送先</MenuItem>
+        <MenuItem onClick={ () => handleMenuSelect('ユーザー') }>ユーザー</MenuItem>
+        <MenuItem onClick={ () => handleMenuSelect('ロール') }>ロール</MenuItem>
       </Menu>
       <CustomTabPanel value={ tabIndex } index={ 0 }>
-        <div>Dashboard コンテンツ</div>
+        { !stockMenu && <div>在庫メニューを選択してください</div> }
+        { stockMenu && stockComponents[stockMenu] }
       </CustomTabPanel>
       <CustomTabPanel value={tabIndex} index={1}>
         { !masterMenu && <div>マスタを選択してください</div> }
