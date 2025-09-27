d81c3cab2c58c445724b7baeb1862ecd8d6061a8
n-nob-at-GitHub <sub.nakamura.xyz@gmail.com>
2025-09-27 22:41:31 +0900

    全社用簡易Webページ対応（デプロイエラーが発生しないかの確認）
    next.config.ts に output: 'export' を 指定
    package.json から next export を 削除

--- simple-busininal-webpage/next.config.ts
+++ simple-busininal-webpage/next.config.ts
@@ -1,13 +1,11 @@
 import type { NextConfig } from 'next';
 
-const isProd = process.env.NODE_ENV === 'production'
-
 const nextConfig: NextConfig = {
   // https://nextjs.org/docs/app/api-reference/config/next-config-js/devIndicators#appisrstatus-static-indicator
   devIndicators: false,
   // https://qiita.com/unreadabread/items/920420c24fc49cb3b392
   // Use Static Export when running next build.
-  output: isProd ? 'export' : undefined,
+  output: 'export',
   // https://nextjs.org/docs/app/api-reference/config/next-config-js/basePath
   // basePath: isProd ? '/external-arte-hall-koushoin' : '',
   // https://nextjs.org/docs/app/api-reference/config/next-config-js/assetPrefix
