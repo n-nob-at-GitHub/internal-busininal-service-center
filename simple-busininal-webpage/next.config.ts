b190cf27bc457ae9dc8d4faec6dedab2b914994f
n-nob-at-GitHub <sub.nakamura.xyz@gmail.com>
2025-09-27 23:05:06 +0900

    全社用簡易Webページ対応（デプロイエラーが発生しないかの確認）
    nextjs.yml は, run: aws s3 sync simple-busininal-webpage s3://${{ secrets.AWS_S3_BUCKET }} --delete に修正

--- simple-busininal-webpage/next.config.ts
+++ simple-busininal-webpage/next.config.ts
@@ -1,11 +1,13 @@
 import type { NextConfig } from 'next';
 
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig: NextConfig = {
   // https://nextjs.org/docs/app/api-reference/config/next-config-js/devIndicators#appisrstatus-static-indicator
   devIndicators: false,
   // https://qiita.com/unreadabread/items/920420c24fc49cb3b392
   // Use Static Export when running next build.
-  output: 'export',
+  output: isProd ? 'export' : undefined,
   // https://nextjs.org/docs/app/api-reference/config/next-config-js/basePath
   // basePath: isProd ? '/external-arte-hall-koushoin' : '',
   // https://nextjs.org/docs/app/api-reference/config/next-config-js/assetPrefix
