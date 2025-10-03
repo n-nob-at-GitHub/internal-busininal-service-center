71f5d64ff5d6ee22cd2d02db52465eead975ba34
n-nob-at-GitHub <sub.nakamura.xyz@gmail.com>
2025-10-03 09:15:37 +0900

    全社用簡易Webページ対応（Cognitoユーザー表示確認, 環境変数（Repository secrets）が取れているか確認）

--- simple-busininal-webpage/src/lib/amplify.ts
+++ simple-busininal-webpage/src/lib/amplify.ts
@@ -1,5 +1,11 @@
 import { Amplify } from 'aws-amplify'
 
+console.log('NEXT_PUBLIC_COGNITO_USER_POOL_ID:', process.env.NEXT_PUBLIC_COGNITO_USER_POOL_ID)
+console.log('NEXT_PUBLIC_REDIRECT_URI:',         process.env.NEXT_PUBLIC_REDIRECT_URI)
+console.log('NEXT_PUBLIC_USER_POOL_CLIENT_ID:',  process.env.NEXT_PUBLIC_USER_POOL_CLIENT_ID)
+console.log('NEXT_PUBLIC_USER_POOL_DOMAIN:',     process.env.NEXT_PUBLIC_USER_POOL_DOMAIN)
+console.log('NEXT_PUBLIC_USER_POOL_REGION:',     process.env.NEXT_PUBLIC_USER_POOL_REGION)
+
 Amplify.configure({
   Auth: {
     Cognito: {
