6b89af2f15a311646d69610e4b993d724919e385
n-nob-at-GitHub <sub.nakamura.xyz@gmail.com>
2025-10-03 20:00:53 +0900

    全社用簡易Webページ対応（本番API作成の準備, user/index.jsは, 後で修正予定, 以下のポリシーを追加して, GitHub Actionsが動作するかチェック）
    lambda-policy-for-github-actions
    {
        "Version": "2012-10-17",
        "Statement": [
            {
                "Sid": "VisualEditor0",
                "Effect": "Allow",
                "Action": "lambda:UpdateFunctionCode",
                "Resource": "arn:aws:lambda:ap-northeast-1:XXXXXXXXXXXX:function:*"
            }
        ]
    }

--- lambda/user/index.js
+++ lambda/user/index.js
@@ -1,1 +1,7 @@
-1
+export const handler = async (event) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify('This is updated by GitHub Actions.'),
+  };
+  return response;
+};
