ae74cbfc33eb084a414174ff872cfd1f2511813f
n-nob-at-GitHub <sub.nakamura.xyz@gmail.com>
2025-10-03 10:20:14 +0900

    全社用簡易Webページ対応（Cognitoユーザー表示確認, 環境変数（Repository secrets）が取れているか確認）

--- simple-busininal-webpage/src/components/Header.tsx
+++ simple-busininal-webpage/src/components/Header.tsx
@@ -1,10 +1,16 @@
 'use client'
-import React from 'react'
+import React, { useEffect } from 'react'
 import Image from 'next/image'
 import logo from '@/images/logo.webp'
+import { configureAmplify } from '@/lib/amplify'
 import { useUser } from '@/hooks/useUser'
 
 const Header = () => {
+  useEffect(() => {
+    console.log('----- Header configureAmplify start. -----')
+    configureAmplify()
+    console.log('----- Header configureAmplify end. -----')
+  }, [])
   const user = useUser()
   return (
     <div style={{
