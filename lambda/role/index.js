c146fa62a79b42c907bd9eb02559620c25763c2c
n-nob-at-GitHub <sub.nakamura.xyz@gmail.com>
2025-10-08 17:52:38 +0900

    全社用簡易Webページ対応（本番API作成の準備, CORS設定を追記, 以下のエラー回避対策） 'Access-Control-Allow-Credentials': 'true',
    Access to XMLHttpRequest at 'https://aoby2arsjj.execute-api.ap-northeast-1.amazonaws.com/role' from origin 'https://d2slubzovll4xp.cloudfront.net' has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource.
    719-f6fe72c65f27061b.js:8  GET https://aoby2arsjj.execute-api.ap-northeast-1.amazonaws.com/role net::ERR_FAILED 200 (OK)

--- lambda/role/index.js
+++ lambda/role/index.js
@@ -14,6 +14,7 @@ const CORS_HEADERS = {
   'Access-Control-Allow-Origin': 'https://d2slubzovll4xp.cloudfront.net/',
   'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE',
   'Access-Control-Allow-Headers': 'Content-Type',
+  'Access-Control-Allow-Credentials': 'true',
 };
 
 exports.handler = async (event) => {
