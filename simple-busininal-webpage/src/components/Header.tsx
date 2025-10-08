320c17add54eef94b1a76fe8669b6c404cca4f2e
n-nob-at-GitHub <sub.nakamura.xyz@gmail.com>
2025-10-08 13:10:15 +0900

    全社用簡易Webページ対応（Cognitoユーザー表示確認, ログアウト機能追加）

--- simple-busininal-webpage/src/components/Header.tsx
+++ simple-busininal-webpage/src/components/Header.tsx
@@ -1,11 +1,29 @@
 'use client'
-import React from 'react'
+import React, { useState } from 'react'
 import Image from 'next/image'
 import logo from '@/images/logo.webp'
 import { useUser } from '@/hooks/useUser'
+import { useAuth } from 'react-oidc-context'
+import ConfirmDialog from '@/app/ConfirmDialog'
+import { YesOrNo } from '@/types/YesOrNo'
 
 const Header = () => {
   const user = useUser()
+  const auth = useAuth()
+  const [ isDialogOpen, setIsDialogOpen ] = useState(false)
+  const handleUserClick = () => {
+    setIsDialogOpen(true)
+  }
+  const handleDialogClose = async () => {
+    setIsDialogOpen(false)
+  }
+  const handleJudge = (result: YesOrNo) => {
+    setIsDialogOpen(false)
+    if (result === 'Yes') {
+      auth.signoutRedirect()
+    }
+  }
+
   return (
     <div style={{
         display: 'flex',
@@ -25,9 +43,20 @@ const Header = () => {
         style={{ objectFit: 'contain' }}
       />
       </a>
-      <div style={{ fontSize: '30px' }}>
+      <div
+        style={{ fontSize: '30px', cursor: 'pointer' }}
+        onClick={ handleUserClick }
+        title='クリックでログアウト'
+      >
         { user ? `${ user.name }@${ user.role }` : 'loading...' }
       </div>
+      <ConfirmDialog
+        isOpen={ isDialogOpen }
+        title='ログアウト確認'
+        message='ログアウトしてもよろしいですか？'
+        judge={ handleJudge }
+        onClose={ handleDialogClose }
+      />
     </div>
   )
 }
