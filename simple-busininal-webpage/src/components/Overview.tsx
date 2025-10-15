affed01f6c937d37f1fabeb726f58c42f5741454
n-nob-at-GitHub <sub.nakamura.xyz@gmail.com>
2025-10-16 07:07:02 +0900

    全社用簡易Webページ対応（本番環境整備, アプリ機能説明に追記）

--- simple-busininal-webpage/src/components/Overview.tsx
+++ simple-busininal-webpage/src/components/Overview.tsx
@@ -9,12 +9,12 @@ import {
   AccordionSummaryProps,
   accordionSummaryClasses,
   Box,
+  Button,
   Card,
   CardHeader,
   CardContent,
   List,
   ListItem,
-  ListItemText,
   Table,
   TableBody,
   TableCell,
@@ -80,24 +80,24 @@ const sections = [
     content: (
       <>
         <Card sx={{ mt: 0 }}>
-            <CardHeader
-              title='ユーザー'
-              sx={{
-                bgcolor: '#f5f5f5',
-                borderBottom: '1px solid #e0e0e0',
-                py: 1
-              }}
-            />
-            <CardContent>
-              <Typography>アプリで利用可能な機能は、ユーザーの権限（ロール）ごとに異なる。</Typography>
-              <Typography>ユーザーは、ロールで、以下のように分類する。</Typography>
-              <ul>
-                <li>SYSTEM</li>
-                <li>ADMIN</li>
-                <li>STAFF</li>
-              </ul>
-              <Typography>アプリの機能は、大別して、マスタ、在庫管理である。</Typography>
-            </CardContent>
+          <CardHeader
+            title='ユーザー'
+            sx={{
+              bgcolor: '#f5f5f5',
+              borderBottom: '1px solid #e0e0e0',
+              py: 1
+            }}
+          />
+          <CardContent>
+            <Typography>アプリで利用可能な機能は、ユーザーの権限（ロール）ごとに異なる</Typography>
+            <Typography>ユーザーは、ロールで分類</Typography>
+            <ul>
+              <li>SYSTEM</li>
+              <li>ADMIN</li>
+              <li>STAFF</li>
+            </ul>
+            <Typography>アプリの機能は、マスタ、在庫管理の二つ</Typography>
+          </CardContent>
         </Card>
         <Card sx={{ mt: 2 }}>
           <CardHeader
@@ -202,6 +202,67 @@ const sections = [
                   <TableCell align='center'>〇</TableCell>
                   <TableCell align='center'>〇</TableCell>
                   <TableCell align='center'>✕</TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </CardContent>
+        </Card>
+      </>
+    ),
+  },
+  {
+    key: 'authentication',
+    title: '認証',
+    content: (
+      <>
+        <Card sx={{ mt: 2 }}>
+          <CardHeader
+            title='認証情報'
+            sx={{
+              bgcolor: '#f5f5f5',
+              borderBottom: '1px solid #e0e0e0',
+              py: 1
+            }}
+          />
+          <CardContent sx={{ py: 0 }}>
+            <Table size='small' sx={{ mt: 1 }}>
+              <TableHead>
+                <TableRow>
+                  <TableCell>項目</TableCell>
+                  <TableCell align='center'>内容</TableCell>
+                  <TableCell align='center'>備考</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                <TableRow>
+                  <TableCell>ログインURL</TableCell>
+                  <TableCell align='left'>https://d2slubzovll4xp.cloudfront.net/</TableCell>
+                  <TableCell align='left'>Amazon Cognito Hosted UIを利用</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>初期パスワード</TableCell>
+                  <TableCell align='left'>InitPass123!</TableCell>
+                  <TableCell align='left'>ユーザー登録後に必要</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>パスワードリセット</TableCell>
+                  <TableCell align='left'>Forgot your password? のリンクから、誘導に従って対応して下さい</TableCell>
+                  <TableCell align='left'>初期パスワード変更済みであること、メールアドレスが使えることが必要</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>Access Tokenの有効期限</TableCell>
+                  <TableCell align='left'>60分</TableCell>
+                  <TableCell align='left'>有効期限過ぎたらログイン画面に遷移</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>F5リロード</TableCell>
+                  <TableCell align='left'>Authentication error: ... のような表示が出たら <Button variant='outlined'>Retry</Button> ボタンを押下</TableCell>
+                  <TableCell align='left'>ログイン画面、または、トップ画面に遷移</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>認証エラー</TableCell>
+                  <TableCell align='left'>例えば、Application error: a client-side exception has occurred while loading ... のようなエラーが出たらシステム管理者に報告</TableCell>
+                  <TableCell align='left'></TableCell>
                 </TableRow>
               </TableBody>
             </Table>
