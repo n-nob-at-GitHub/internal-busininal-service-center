0d798595479445052bb2dd4b76915815eee6dc1a
n-nob-at-GitHub <sub.nakamura.xyz@gmail.com>
2025-10-16 18:42:44 +0900

    全社用簡易Webページ対応（本番環境整備, アプリ機能説明に追記）

--- simple-busininal-webpage/src/components/Overview.tsx
+++ simple-busininal-webpage/src/components/Overview.tsx
@@ -83,27 +83,25 @@ const sections = [
           <CardHeader
             title='ユーザー'
             sx={{
-              bgcolor: '#f5f5f5',
+              bgcolor: '#e3f2fd',
               borderBottom: '1px solid #e0e0e0',
               py: 1
             }}
           />
           <CardContent>
-            <Typography>アプリで利用可能な機能は、ユーザーの権限（ロール）ごとに異なる</Typography>
-            <Typography>ユーザーは、ロールで分類</Typography>
+            <Typography>アプリで利用可能な機能は、以下のユーザーの権限（ロール）ごとに異なる</Typography>
             <ul>
               <li>SYSTEM</li>
               <li>ADMIN</li>
               <li>STAFF</li>
             </ul>
-            <Typography>アプリの機能は、マスタ、在庫管理の二つ</Typography>
           </CardContent>
         </Card>
         <Card sx={{ mt: 2 }}>
           <CardHeader
             title='マスタ'
             sx={{
-              bgcolor: '#f5f5f5',
+              bgcolor: '#e3f2fd',
               borderBottom: '1px solid #e0e0e0',
               py: 1
             }}
@@ -157,7 +155,7 @@ const sections = [
           <CardHeader
             title='在庫管理'
             sx={{
-              bgcolor: '#f5f5f5',
+              bgcolor: '#e3f2fd',
               borderBottom: '1px solid #e0e0e0',
               py: 1
             }}
@@ -219,7 +217,7 @@ const sections = [
           <CardHeader
             title='認証情報'
             sx={{
-              bgcolor: '#f5f5f5',
+              bgcolor: '#e3f2fd',
               borderBottom: '1px solid #e0e0e0',
               py: 1
             }}
@@ -246,8 +244,8 @@ const sections = [
                 </TableRow>
                 <TableRow>
                   <TableCell>パスワードリセット</TableCell>
-                  <TableCell align='left'>Forgot your password? のリンクから、誘導に従って対応して下さい</TableCell>
-                  <TableCell align='left'>初期パスワード変更済みであること、メールアドレスが使えることが必要</TableCell>
+                  <TableCell align='left'>Forgot your password? のリンクから、誘導に従って進む</TableCell>
+                  <TableCell align='left'>①初期パスワード変更済みであること、②メールアドレスが使えること、の両方が必要</TableCell>
                 </TableRow>
                 <TableRow>
                   <TableCell>Access Tokenの有効期限</TableCell>
@@ -259,8 +257,178 @@ const sections = [
                   <TableCell align='left'>Authentication error: ... のような表示が出たら <Button variant='outlined'>Retry</Button> ボタンを押下</TableCell>
                   <TableCell align='left'>ログイン画面、または、トップ画面に遷移</TableCell>
                 </TableRow>
+              </TableBody>
+            </Table>
+          </CardContent>
+        </Card>
+      </>
+    ),
+  },
+  {
+    key: 'inbound-outbound',
+    title: '入出庫',
+    content: (
+      <>
+        <Card sx={{ mt: 2 }}>
+          <CardHeader
+            title='入庫'
+            sx={{
+              bgcolor: '#e3f2fd',
+              borderBottom: '1px solid #e0e0e0',
+              py: 1
+            }}
+          />
+          <CardContent sx={{ py: 1 }}>
+            <List sx={{ listStyle: 'decimal', pl: 4, pb: 0 }}>
+              {[
+                '在庫管理タブ → 入庫 から 入庫対象品を表示',
+                '各資材の入庫数を入力',
+                <>右上のアイコン <DataSaverOnOutlinedIcon sx={{ verticalAlign: 'middle' }} /> を押下</>,
+                '入庫確認ダイアログで、OKボタンを押下',
+              ].map((step, i) => (
+                <StepItem key={ i }>
+                  { step }
+                </StepItem>
+              ))}
+            </List>
+          </CardContent>
+        </Card>
+        <Card sx={{ mt: 2 }}>
+          <CardHeader
+            title='入庫取消'
+            sx={{
+              bgcolor: '#e3f2fd',
+              borderBottom: '1px solid #e0e0e0',
+              py: 1
+            }}
+          />
+          <CardContent sx={{ py: 1 }}>
+            <List sx={{ listStyle: 'decimal', pl: 4, pb: 0 }}>
+              {[
+                '在庫管理タブ → 入庫履歴 から 入庫履歴を表示',
+                '該当するNo.の入庫で、無効に切り替える',
+              ].map((step, i) => (
+                <StepItem key={ i }>
+                  { step }
+                </StepItem>
+              ))}
+            </List>
+          </CardContent>
+        </Card>
+        <Card sx={{ mt: 2 }}>
+          <CardHeader
+            title='出庫'
+            sx={{
+              bgcolor: '#e3f2fd',
+              borderBottom: '1px solid #e0e0e0',
+              py: 1
+            }}
+          />
+          <CardContent sx={{ py: 1 }}>
+            <List sx={{ listStyle: 'decimal', pl: 4, pb: 0 }}>
+              {[
+                '在庫管理タブ → 出庫 から 出庫対象品を表示',
+                '配送先を選択',
+                '各資材の出庫数を入力',
+                <>右上のアイコン <DataSaverOnOutlinedIcon sx={{ verticalAlign: 'middle' }} /> を押下</>,
+                '出庫確認ダイアログで、OKボタンを押下',
+              ].map((step, i) => (
+                <StepItem key={ i }>
+                  { step }
+                </StepItem>
+              ))}
+            </List>
+          </CardContent>
+        </Card>
+        <Card sx={{ mt: 2 }}>
+          <CardHeader
+            title='出庫取消'
+            sx={{
+              bgcolor: '#e3f2fd',
+              borderBottom: '1px solid #e0e0e0',
+              py: 1
+            }}
+          />
+          <CardContent sx={{ py: 1 }}>
+            <List sx={{ listStyle: 'decimal', pl: 4, pb: 0 }}>
+              {[
+                '在庫管理タブ → 出庫履歴 から 出庫履歴を表示',
+                '該当するNo.の出庫で、無効に切り替える',
+              ].map((step, i) => (
+                <StepItem key={ i }>
+                  { step }
+                </StepItem>
+              ))}
+            </List>
+          </CardContent>
+        </Card>
+      </>
+    ),
+  },
+  {
+    key: 'other',
+    title: 'その他',
+    content: (
+      <>
+        <Card sx={{ mt: 2 }}>
+          <CardHeader
+            title='制約事項'
+            sx={{
+              bgcolor: '#ffe6eb',
+              borderBottom: '1px solid #e0e0e0',
+              py: 1
+            }}
+          />
+          <CardContent sx={{ py: 0 }}>
+            <Table size='small' sx={{ mt: 1 }}>
+              <TableHead>
                 <TableRow>
-                  <TableCell>認証エラー</TableCell>
+                  <TableCell>項目</TableCell>
+                  <TableCell align='center'>内容</TableCell>
+                  <TableCell align='center'>備考</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                <TableRow>
+                  <TableCell>ログアウト</TableCell>
+                  <TableCell align='left'>本アプリでは明示的なログアウトの導線は無し、Access Tokenの有効期限を過ぎたらログアウトと見做す</TableCell>
+                  <TableCell align='left'>Amazon Cognito（認証基盤）の仕様に従うため</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>入出庫金額の不正確性</TableCell>
+                  <TableCell align='left'>例えば、12個で998円のような資材の入出庫は、正確な単価を設定できない（約83.167円）ため、金額部分が不正確になる</TableCell>
+                  <TableCell align='left'>但し、資材を分割出庫せず、常に購入単位で出庫する運用であれば、この限りではない</TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </CardContent>
+        </Card>
+        <Card sx={{ mt: 2 }}>
+          <CardHeader
+            title='QA'
+            sx={{
+              bgcolor: '#fff9c4',
+              borderBottom: '1px solid #e0e0e0',
+              py: 1
+            }}
+          />
+          <CardContent sx={{ py: 0 }}>
+            <Table size='small' sx={{ mt: 1 }}>
+              <TableHead>
+                <TableRow>
+                  <TableCell>項目</TableCell>
+                  <TableCell align='center'>内容</TableCell>
+                  <TableCell align='center'>備考</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                <TableRow>
+                  <TableCell>複数ユーザーでの操作</TableCell>
+                  <TableCell align='left'>ブラウザをシークレットモードで開き、本アプリにログイン</TableCell>
+                  <TableCell align='left'>通常、想定しない使い方（システム管理者などが利用する可能性あり）</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>認証エラーが出た場合</TableCell>
                   <TableCell align='left'>例えば、Application error: a client-side exception has occurred while loading ... のようなエラーが出たらシステム管理者に報告</TableCell>
                   <TableCell align='left'></TableCell>
                 </TableRow>
@@ -270,47 +438,6 @@ const sections = [
         </Card>
       </>
     ),
-  },
-  {
-    key: 'inbound',
-    title: '入庫',
-    steps: [
-      '在庫管理 → 入庫 から 入庫対象品を表示',
-      '各資材の入庫数を入力',
-      <>
-        右上のアイコン <DataSaverOnOutlinedIcon /> を押下
-      </>,
-      '入庫確認ダイアログで、OKボタンを押下',
-    ],
-  },
-  {
-    key: 'inbound-cancel',
-    title: '入庫取消',
-    steps: [
-      '在庫管理 → 入庫履歴 から 入庫履歴を表示',
-      '該当するNo.の入庫で、無効に切り替える',
-    ],
-  },
-  {
-    key: 'outbound',
-    title: '出庫',
-    steps: [
-      '在庫管理 → 出庫 から 出庫対象品を表示',
-      '配送先を選択',
-      '各資材の出庫数を入力',
-      <>
-        右上のアイコン <DataSaverOnOutlinedIcon /> を押下
-      </>,
-      '出庫確認ダイアログで、OKボタンを押下',
-    ],
-  },
-  {
-    key: 'outbound-cancel',
-    title: '出庫取消',
-    steps: [
-      '在庫管理 → 出庫履歴 から 出庫履歴を表示',
-      '該当するNo.の出庫で、無効に切り替える',
-    ],
   },
 ];
 
@@ -324,7 +451,7 @@ const Overview = () => {
 
   return (
     <div>
-      {sections.map(({ key, title, content, steps }) => (
+      { sections.map(({ key, title, content }) => (
         <Accordion
           key={ key }
           expanded={ expanded === key }
@@ -335,15 +462,6 @@ const Overview = () => {
           </AccordionSummary>
           <AccordionDetails>
             { content }
-            { steps && (
-              <List sx={{ listStyle: 'decimal', pl: 4 }}>
-                { steps.map((step, i) => (
-                  <StepItem key={ i }>
-                    { step }
-                  </StepItem>
-                ))}
-              </List>
-            )}
           </AccordionDetails>
         </Accordion>
       ))}
